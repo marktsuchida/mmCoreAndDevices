c8e9d4bb2db6f0b9952f9b580795a82a31ceb114
Mark A. Tsuchida <matsuchida@wisc.edu>
2025-12-08 16:28:18 -0600

    Was missing ToString() for storage device type

--- MMCore/CoreUtils.h
+++ MMCore/CoreUtils.h
@@ -72,6 +72,7 @@ inline std::string ToString(const MM::DeviceType d)
       case MM::SLMDevice: return "SLM";
       case MM::HubDevice: return "Hub";
       case MM::GalvoDevice: return "Galvo";
+      case MM::StorageDevice: return "Storage";
    }
    return "Invalid";
 }
